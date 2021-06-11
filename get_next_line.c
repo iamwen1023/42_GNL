a57c3be7d10e7e8f1d6f1451a68d28b21e5b32f8
Wen-tzu LO <wlo@e3r3p20.42.fr>
2021-06-11 17:31:08 +0200

    over 25 lines

--- get_next_line.c
+++ get_next_line.c
@@ -113,26 +113,26 @@ int get_next_line(int fd, char **line)
     return (1);
 }
 
-// int main()
-// {
-//     int     fd;
-// 	char	*line;
-// 	int     i;
+int main()
+{
+    int     fd;
+	char	*line;
+	int     i;
 
-// 	fd = open("try", O_RDONLY);
+	fd = open("try", O_RDONLY);
 
-//     while ((i = get_next_line(fd, &line)) > 0)
-// 	{
-// 	    printf("\e[33mline READ:\e[0m\n|%s|\n", line);
-//         free(line);
-// 	}
-// 	if (i == -1)
-//         printf("\e[31mError encountered\e[0m\n");
-//     else if (!i)
-//         printf("\e[32mEOF reached\e[0m\n|%s|\n", line);
-//     else
-//         printf("Unexpected return of get_next_line\ni == %d\n", i);
-// 	close(fd);
-//    // system("leaks a.out");
-//     return (0);
-// }
+    while ((i = get_next_line(fd, &line)) > 0)
+	{
+	    printf("\e[33mline READ:\e[0m\n|%s|\n", line);
+        free(line);
+	}
+	if (i == -1)
+        printf("\e[31mError encountered\e[0m\n");
+    else if (!i)
+        printf("\e[32mEOF reached\e[0m\n|%s|\n", line);
+    else
+        printf("Unexpected return of get_next_line\ni == %d\n", i);
+	close(fd);
+   // system("leaks a.out");
+    return (0);
+}
